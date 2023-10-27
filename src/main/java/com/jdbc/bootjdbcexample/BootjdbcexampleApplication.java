ffce49d2a9a11bc39220bc2dd14a2a08bd78eb3d
Diya Jayakumar <diyajaikumar2000@gmail.com>
2023-10-27 12:31:16 +0530

    spring_jdbc

---
+++ src/main/java/com/jdbc/bootjdbcexample/BootjdbcexampleApplication.java
@@ -0,0 +1,44 @@
+package com.jdbc.bootjdbcexample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@SpringBootApplication
+public class BootjdbcexampleApplication implements CommandLineRunner {
+	@Autowired
+	private UserDao userDao;
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootjdbcexampleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(this.userDao.createTable());
+		this.createUser();
+	}
+	public void createUser() throws IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("enter user id");
+		Integer userId = Integer.parseInt(br.readLine());
+
+		System.out.println("enter user name");
+		String name = br.readLine();
+
+		System.out.println("enter user age");
+		Integer age = Integer.parseInt(br.readLine());
+
+		System.out.println("enter user city");
+		String city = br.readLine();
+
+		int i= this.userDao.insertUser(userId,name,age,city);
+		System.out.println(i+"user added");
+	}
+
+}
---
+++ src/main/java/com/jdbc/bootjdbcexample/UserDao.java
@@ -0,0 +1,28 @@
+package com.jdbc.bootjdbcexample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public class UserDao {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    public UserDao(){
+
+    }
+    public int createTable(){
+        String query="CREATE TABLE IF NOT EXISTS User(id int primary key, name varchar(200) , age int, city varchar(100))";
+        int update= this.jdbcTemplate.update(query);
+        return update;
+    }
+    public int insertUser(Integer id, String name, Integer age, String city){
+        String query= "insert into user(id,name,age,city) values(?,?,?,?)";
+
+        int update= this.jdbcTemplate.update(query,new Object[]{id,name,age,city});
+        return update;
+    }
+
+}
+
